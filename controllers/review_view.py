cbc489936e3014c7c33c2eb176311e7a4e0f6090
Suyu Sang <sssang@umich.edu>
2015-12-10 14:38:46 -0500

    finish except user & menu

--- controllers/review_view.py
+++ controllers/review_view.py
@@ -2,6 +2,7 @@ from flask.views import MethodView
 from flask import render_template, redirect, url_for, request
 
 from models.restuarant import Rest
+from models.review import Review
 
 class ReviewAPI(MethodView):
     def get(self):
@@ -9,6 +10,17 @@ class ReviewAPI(MethodView):
         rests = Rest.get_rest_object(restid)
 
         return render_template('review.html', rests=rests)
-        
+
     def post(self):
-        pass
+        op = request.form.get('op')
+        if op == 'search':
+            keyword = request.form.get('keyword')
+            return redirect(url_for('search_result')+'?keyword='+keyword)
+        elif op == 'review':
+            restid = request.form.get('restid')
+            rating = request.form.get('rating')
+            content = request.form.get('review_content')
+            country = request.form.get('country')
+
+            Review.insert(restid, rating, content, country)
+            return redirect(url_for('rest_index')+'?id='+restid)
--- models/review.py
+++ models/review.py
@@ -8,6 +8,7 @@ class Review(object):
         self.created = created,
         self.lastupdated = lastupdated
         self.country=country
+
     @classmethod
     def get_rest_reviews(cls, restid):
         connection = mysql.connect()
@@ -20,3 +21,40 @@ class Review(object):
         cursor.close()
         connection.close()
         return [cls(*t) for t in data] if data else []
+
+    @classmethod
+    def insert(cls, restid, rating, content, country):
+        connection = mysql.connect()
+        cursor = connection.cursor()
+
+        sql = "insert into Review(rating, content, country) values (%s, %s, %s)"
+        cursor.execute(sql, (rating, content, country))
+        connection.commit()
+
+        cursor.execute("select revid from Review order by revid desc")
+        revid = cursor.fetchall()
+        revid = revid[0][0]
+
+        cursor.execute("select sequencenum from ReviewContain where restid=%s order by sequencenum desc"%restid)
+        sn = cursor.fetchall()
+        sn = sn[0][0]+1 if sn else 1
+
+        sql = "insert into ReviewContain(restid, revid, sequencenum) values (%s, %s, %s)"
+        cursor.execute(sql, (restid, revid, sn))
+        connection.commit()
+
+        sql = "select rating from CountryRating where restid=%s and country=%s"
+        cursor.execute(sql, (restid, country))
+        data = cursor.fetchall()
+        if not data:
+            rating_new = rating
+            sql = "insert into CountryRating(rating, restid, country) values (%s, %s, %s)"
+        else:
+            rating_new = (data[0][0] + rating)*0.5
+            sql = "update CountryRating set rating=%s where restid=%s and country=%s"
+
+        cursor.execute(sql, (rating_new, restid, country))
+        connection.commit()
+
+        cursor.close()
+        connection.close()
