b7e654191a9af976e20f718c4b3154b74abba0f3
Zijia Cai <zijcai@umich.edu>
2015-12-10 02:54:18 -0500

    update load_data

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -4,6 +4,49 @@ values ("Angelo's", "static/pictures/angelos.jpg", "Breakfast & Brunch","Family-
 insert into Rest (restname, picurl, resttag,description, rating, address,  city,state,country)
 values ("Zingerman's", "static/pictures/zingerman.jpg", "Delis, Breakfast & Brunch","Zingerman's, or Zingerman's Community of Businesses, is a gourmet food business group headquartered in Ann Arbor, Michigan, USA. The original business and current flagship operation is Zingerman's Delicatessen. ", "4.5", "422 Detroit St, Ann Arbor, MI 48104", "ann arbor","michigan","united stats");
 
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("Sava's","http://bigpanlittlepan.com/wp-content/uploads/2015/05/45-percent-sava-header-logo.png","Local restaurant","A local sanctuary where community, culture, and craft meet for a truly authentic culinary experience.","4.5","216 SOUTH STATE STREET ANN ARBOR, MI 88104","ann arbor","michigan","united state");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("Lucky Kitchen", "http://www.luckykitchen.net/images/index2_03.gif","authentic chinese food","Lucky Kitchen Chinese Restaurant, Ann Arbor, MI - LKfit Meals, takeout, dine-in and delivery from two locations near U of M. Authentic Chinese off-premise catering from appetizers to full meals is the healthy, economical way to feed any group.","3","611 E University Ave, Ann Arbor, MI 48104","ann arbor","michigan","united state");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("Evergreen","http://nebula.wsimg.com/01789fb1d98ed85afb8dae5da57d1648?AccessKeyId=49806A5A056E8BC7E822&disposition=0&alloworigin=1","Authentic Chinese Food","Since 1992, we have consistently served Ann Arbor with delicious, gourmet Chinese cuisine.  Specializing in Mandarin, Sizchuan, Hunan, Shanghai and Taiwan styles of cooking, Evergreen promises only the most authentic flavors with the freshest ingredients, and always at a affordable price.","5","2771 Plymouth Rd. Ann Arbor MI 48105 US","ann arbor","michigan","united state");
+
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("The Upper West","http://www.theupperwest.com/wp-content/uploads/2014/05/250x54b.png","American Cuisine","With a constantly changing menu, Upper West boasts of Chef Nick Shipp’s eclectic individualistic dishes that tickle the palate with unique ingredients from all around the world. The restaurant reflects an uncompromising approach to modern American cuisine that knows no bounds coupled with exceptional customer service.","4.2","3321 Pico Blvd, Santa Monica, CA, 90404","Santa Monica","california","united state");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("Cha Cha Chicken","http://chachachicken.com/chachachicken7.jpg","caribbean cuisine","Caribbean cuisine with Latin fusion","2.0","1906 Ocean Ave,Santa Monica,CA, 90405","Santa Monica","california","united state");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("Meisse","http://melisse.com/wp-content/themes/melisse-theme/images/logo-melisse.png","Michilin star restaurant","Situated in Santa Monica, Mélisse inspires all of the senses with its chic ambiance, superb, contemporary French cuisine and gracious service. The Olivia and Augustin Rooms offer privacy for intimate gatherings or business entertaining","5","1104 WILSHIRE BLVD, SANTA MONICA, CA 90401","santa monica","california","united states");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("The IVY","http://www.theivyrestaurants.com/images/slider/the-ivy/1.jpg","Perfect Brunch Place","","3.5","1535 Ocean Ave, Santa Monica, CA 90401","santa monica","california","united state");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("Zabies","http://www.zabies.com/images/menu-breakfast.png","Breakfast cafe","At Zabie’s Neighborhood Cafe we hand pick our ingredients almost daily to ensure we deliver high quality, fresh and delicious meals to our guests. We are a gourmet café offering a variety of healthy breakfast and lunch options as well as staple comfort foods.","3.0","3003 Ocean Park Blvd. Santa Monica, CA 90405","santa monica","california","united state");
+
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("Nebbiolo Ristorante","https://www.visualitineraries.com/img/locations/20002.jpg","secrets the Vine","","4","Blvd. Kukulcan Km. 14.5 Lote 38, Zona Hotelera, Cancun 77500, Mexico","cancun","","mexico");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("The White Box", "http://media-cdn.tripadvisor.com/media/photo-s/05/77/2f/c7/the-white-box.jpg", "High-end Dining","Located in the Grand Oasis Cancun, The White Box is a gastrobar created by Chef Rafael Zafra; who, among his many accomplishments, worked alongside Chef Ferran Adria as part of the remarkable team behind five times best restaurant in the world, El Bulli. ", "4.5", "Blvd. Kukulcán Km 16.5, Lote 45, 46 y 47, Zona Hotelera, 77500 Cancún, Mexico", "cancun","","mexico");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("Tempo by Martin Berasategui", "http://tempobymartinberasategui.com/wp-content/uploads/2014/05/paradisuscancuntempobco.png", "Michilin style","Tempo by Martin Berasategui is sublime icon of Paradisus Cancun culinary experiences. It is exclusive elegance in an avantgarde. Is carried away by the traditional flavors of the cuisine in a modern concept.", "5", "Blvd. Kukulcan km 16.5 Cancún, México", "cancun","","mexico");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("MB","http://www.junkfoodguy.com/wp-content/uploads/2015/06/liveaqua-mb-05.jpg","high-end dining","The MB Restaurant is the only eating establishment at the Live Aqua that has a dress code – “Resort Chic” - See more at: http://www.junkfoodguy.com/2015/06/11/liveaquamb/#sthash.JcIIuFqw.dpuf","3.4","Boulevard Kukulcan Km. 12.5 at Live Aqua Cancun Hotel, Cancun 77500, Mexico","cancun","","mexico");
+
+insert into Rest (restname, picurl, resttag,description, rating, address, city,state,country)
+values ("STEAKHOUSE PERUVIAN CUISINE","http://lima1205.com.mx/wp-content/uploads/2015/06/LimaColor87x99.png","ocean-view restaurant","With a spectacular view of the Nichupte Lagoon and located in the heart of Cancun’s famed hotel zone, Lima 12 05 Peruvian Cuisine and Steakhouse is the first 100% Peruvian restaurant in all of Southern Mexico.","3.5","Boulevard Kukulkan Km 12. 7, Cancun 77500, Mexico","cancun","","mexico");
+
+
+
 insert into Photo(picid,url)
 values ("1","http://www.zingermansdeli.com/wp-content/uploads/2015/12/NoahsArchetypalReuben-e1449265674501.jpg");
 
@@ -40,6 +83,44 @@ values ("6","http://www.angelosa2.com/themes/angelos/img/photos/food_14.jpg");
 
 insert into PhotoContain(restid,picid,caption,sequencenum)
 values ("1","6","breakfast","3");
+
+insert into Photo(picid,url)
+values ("7","http://s3.amazonaws.com/saveoneverything_assets/assets/images/uploads/sliders/1470351938317b2415.jpg");
+
+insert into PhotoContain(restid,picid,caption,sequencenum)
+values ("4","7","general Tsao's chicken","1");
+
+insert into Photo(picid,url)
+values ("8","http://2.bp.blogspot.com/-XiSJ7wsp0qY/UROYugGsmfI/AAAAAAAAHo4/oYyd4S_FGC8/s640/00000.JPG");
+
+insert into PhotoContain(restid,picid,caption,sequencenum)
+values ("4","8","Fish","2");
+
+insert into Photo(picid,url)
+values ("9","http://www.eatblue.com/original/763cf59525b79b2c2fd079cec7275933_Lucky%20Kitchen.jpg");
+
+insert into PhotoContain(restid,picid,caption,sequencenum)
+values ("4","9","Calamari","3");
+
+
+insert into Photo(picid,url)
+values ("10","http://www.eatparadise.com.au/wp-content/uploads/2014/07/hnxwelkkstei.jpg");
+
+insert into PhotoContain(restid,picid,caption,sequencenum)
+values ("5","10","麻辣香锅","1");
+
+insert into Photo(picid,url)
+values ("11","http://ama.xinshipu.cn/20110429/original/1304032693416.jpg");
+
+insert into PhotoContain(restid,picid,caption,sequencenum)
+values ("5","11","水煮鱼","2");
+
+insert into Photo(picid,url)
+values ("12","https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQiUkOq4D1fLMFEXNL1qQTXvrvaNpCaL3eA5r7ScHSkkxV0WpjToA");
+
+insert into PhotoContain(restid,picid,caption,sequencenum)
+values ("5","12","卤水鸭","3");
+
 
 
 
@@ -82,3 +163,82 @@ values ("3","The service is horrible","ann arbor","michigan","united state");
 
 insert into ReviewContain(restid,revid,sequencenum)
 values ("1","6","3");
+
+
+
+
+
+insert into Review(rating, content,city,state,country)
+values ("5","The FOOD is awesome","ann arbor","michigan","united state");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("4","7","1");
+
+insert into Review(rating, content,city,state,country)
+values ("5","Orange chicken is the best","ann arbor","michigan","united state");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("4","8","2");
+
+insert into Review(rating, content,city,state,country)
+values ("5","Thanks to General Tsao","ann arbor","michigan","united state");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("4","9","3");
+
+insert into Review(rating, content,city,state,country)
+values ("1","This is chinese food?","guangzhou","guangdong","china");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("4","10","4");
+
+insert into Review(rating, content,city,state,country)
+values ("1","The service is horrible","guangzhou","guangdong","china");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("4","11","5");
+
+insert into Review(rating, content,city,state,country)
+values ("1","Everything taste the same lol","guangzhou","guangdong","china");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("4","12","6");
+
+insert into Review(rating, content,city,state,country)
+values ("1","The waiter does not speak english","ann arbor","michigan","united state");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("5","13","1");
+
+insert into Review(rating, content,city,state,country)
+values ("1","*** is this food. They eat everything","ann arbor","michigan","united state");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("5","14","2");
+
+insert into Review(rating, content,city,state,country)
+values ("1","LOL they just serve me chicken feet","ann arbor","michigan","united state");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("5","15","3");
+
+insert into Review(rating, content,city,state,country)
+values ("5","wow the flavour is close to home","guangzhou","guangdong","china");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("5","16","4");
+
+insert into Review(rating, content,city,state,country)
+values ("5","Can not believe they have chicken feet. A must try.","guangzhou","guangdong","china");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("5","17","5");
+
+insert into Review(rating, content,city,state,country)
+values ("5","Damn Some of the dishes even I never eat before. This is awesome","guangzhou","guangdong","china");
+
+insert into ReviewContain(restid,revid,sequencenum)
+values ("5","18","6");
+
+
+
